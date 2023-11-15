dfb1c99d6ba75a238f9889c4309302707a8f7a8c
vburha <142959794+vburha@users.noreply.github.com>
2023-11-15 17:09:13 -0600

    Trying again

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 function init(){  
   function cursorfunction(){
     var cursorarea = document.querySelector('body');
-    var cursorStyle = cursor:
-      url(http://www.rw-designer.com/cursor-extern.php?id=175574);
+    var cursorStyle = cursor: url(.media/sanrio-cinnamoroll.ani);
     cursorarea.style.cssText = cursorStyle;
   }
   var cursorbutton = document.getElementById('cursorbutton');
