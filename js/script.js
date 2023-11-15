a2fbfbacdde1ed758a87b9a6f0ec211537a8ea22
vburha <142959794+vburha@users.noreply.github.com>
2023-11-14 21:01:07 -0600

    Starting my custom cursor idea

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 function init(){
+  var cursorbutton = document.getElementById('cursorbutton');
+  cursorbutton.addEventListener('click', cursorfunction);
   
+  function cursorfunction(){
+    var cursorarea = document.querySelectorAll('body');
+    cursorarea.style.cursor = 'url(http://www.rw-designer.com/cursor-extern.php?id=175574)';
+  }
 }
 window.addEventListener('load', init);
