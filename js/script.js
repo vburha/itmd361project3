0dc8d8a0627fd167eb817bae7cb864601a3d71be
vburha <142959794+vburha@users.noreply.github.com>
2023-11-15 16:51:27 -0600

    Trying again to make it work

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-function init(){
-  var cursorbutton = document.getElementById('cursorbutton');
-  cursorbutton.addEventListener('click', cursorfunction);
-  
+function init(){  
   function cursorfunction(){
-    var cursorarea = document.querySelectorAll('body');
+    var cursorarea = document.querySelector('body');
     cursorarea.style.cursor = 'url(http://www.rw-designer.com/cursor-extern.php?id=175574), auto';
   }
+  var cursorbutton = document.getElementById('cursorbutton');
+  cursorbutton.addEventListener('click', cursorfunction);
 }
 window.addEventListener('load', init);
