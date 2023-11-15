8dd9232faec822ed194cb376a0d0a019a869f80b
vburha <142959794+vburha@users.noreply.github.com>
2023-11-15 17:01:38 -0600

    Tryign again

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 function init(){  
   function cursorfunction(){
     var cursorarea = document.querySelector('body');
-    cursorarea.style.cursor = 'url(http://www.rw-designer.com/cursor-extern.php?id=175574), auto';
+    var cursorStyle = cursor:
+      url(http://www.rw-designer.com/cursor-extern.php?id=175574);
+    cursorarea.style.cssText = cursorStyle;
   }
   var cursorbutton = document.getElementById('cursorbutton');
   cursorbutton.addEventListener('click', cursorfunction);
+  
 }
 window.addEventListener('load', init);
