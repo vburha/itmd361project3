e158950f23d3c21de4aa12fe30d973b9d0bfb7de
vburha <142959794+vburha@users.noreply.github.com>
2023-11-15 22:00:59 -0600

    Trying to fix again

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 function init(){ 
   //Trying to make a custom cursor but it's not working
   function cursorfunction(){
-    document.body.style.cursor = url('.media/cinnamoroll.png'), help;
+    var cursorarea = document.querySelectorAll('body');
+    document.querySelectorAll('body').style.cursor = url('.media/cinnamoroll.png'), help;
   }
   var cursorbutton = document.getElementById('cursorbutton');
   cursorbutton.addEventListener('click', cursorfunction);
