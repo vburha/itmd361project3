b054dd511de5327d7e72486bdf5c2444bfd31bb7
vburha <142959794+vburha@users.noreply.github.com>
2023-11-15 21:45:25 -0600

    Trying again to fix cursor, this time by using a png instead of ani file

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 function init(){ 
   //Trying to make a custom cursor but it's not working
   function cursorfunction(){
-    var cursorarea = document.querySelector('body');
-    cursorarea.style.cursor = url('http://www.rw-designer.com/cursor-extern.php?id=114440'), url('.media/sanrio-cinnamoroll.ani'), help;
+    document.body.style.cursor = url('.media/cinnamoroll.png'), help;
   }
   var cursorbutton = document.getElementById('cursorbutton');
-  cursorbutton.onclick(cursorfunction);
+  cursorbutton.addEventListener('click', cursorfunction);
   
 }
 window.addEventListener('load', init);
