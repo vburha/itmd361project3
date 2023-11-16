b53b1a840c301a9fbbb2a34b1acbc23279e83c01
vburha <142959794+vburha@users.noreply.github.com>
2023-11-16 11:52:02 -0600

    I really don't get why nothing happens o the cursor

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function init(){ 
   //Trying to make a custom cursor but it's not working
   function cursorfunction(){
-    var cursorarea = document.querySelectorAll('body');
-    document.querySelectorAll('body').style.cursor = url('.media/cinnamoroll.png'), crosshair;
+    document.querySelectorAll('body').style = cursor: crosshair;
+    //cursorarea.style.cursor = url('.media/cinnamoroll.png'), crosshair;
   }
   var cursorbutton = document.getElementById('cursorbutton');
   cursorbutton.addEventListener('click', cursorfunction);
