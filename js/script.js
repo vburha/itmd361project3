2e627b937b61ba954883c347514d9dc4162de505
vburha <142959794+vburha@users.noreply.github.com>
2023-11-15 22:06:05 -0600

    Changed the fallback cursor

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function init(){
   //Trying to make a custom cursor but it's not working
   function cursorfunction(){
     var cursorarea = document.querySelectorAll('body');
-    document.querySelectorAll('body').style.cursor = url('.media/cinnamoroll.png'), help;
+    document.querySelectorAll('body').style.cursor = url('.media/cinnamoroll.png'), crosshair;
   }
   var cursorbutton = document.getElementById('cursorbutton');
   cursorbutton.addEventListener('click', cursorfunction);
