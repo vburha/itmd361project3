ff199f8ce13515f0f01a7ddee39dfd719fa2947e
vburha <142959794+vburha@users.noreply.github.com>
2023-11-15 18:33:09 -0600

    Adding fallback cursors to see if I can find the problem

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ function init(){
   //Trying to make a custom cursor but it's not working
   function cursorfunction(){
     var cursorarea = document.querySelector('body');
-    var cursorStyle = cursor: url(.media/sanrio-cinnamoroll.ani);
-    cursorarea.style.cssText = cursorStyle;
+    cursorarea.style.cursor = url('http://www.rw-designer.com/cursor-extern.php?id=114440'), url('.media/sanrio-cinnamoroll.ani'), help;
   }
   var cursorbutton = document.getElementById('cursorbutton');
   cursorbutton.addEventListener('click', cursorfunction);
