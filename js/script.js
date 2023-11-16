13db3c0c6a9f540fa818999d401b907b633cff81
vburha <142959794+vburha@users.noreply.github.com>
2023-11-15 18:37:40 -0600

    Adding alert to further try to detect the problem

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function init(){
     cursorarea.style.cursor = url('http://www.rw-designer.com/cursor-extern.php?id=114440'), url('.media/sanrio-cinnamoroll.ani'), help;
   }
   var cursorbutton = document.getElementById('cursorbutton');
-  cursorbutton.addEventListener('click', cursorfunction);
+  cursorbutton.addEventListener('click', alert('worked'));
   
   //Fullscreen option
   window.requestFullscreen(auto);
