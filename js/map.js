52d75623c9ae3123562e8650ce097fc2b17f5a8d
vburha <142959794+vburha@users.noreply.github.com>
2023-12-05 16:10:27 -0600

    Changing latitude and longitude to see if it works

--- js/map.js
+++ js/map.js
@@ -3,7 +3,7 @@ let map;
 
 async function initMap() {
   // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+  const position = { lat: 41.88278101023464, lng:-87.62333608650817};
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
